9e182d5189574c50e76a999fc4015f66e1a8b7bd
Y77CH <y77chen@outlook.com>
2020-10-11 12:24:19 +0700

    Starts with util pack

---
+++ src/main/java/com/ebquery/util/BasicDataSourceFactory.java
@@ -0,0 +1,56 @@
+package com.EBQuery.util;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BasicDataSourceFactory {
+    private Properties databaseDriverProperties;
+
+    private Map<String, BasicDataSource> dataSources;
+
+    public BasicDataSourceFactory() {
+        dataSources = new HashMap<String, BasicDataSource>();
+    }
+
+    public void setDatabaseDriverProperties(Properties databaseDriverProperties) {
+        this.databaseDriverProperties = databaseDriverProperties;
+    }
+
+    public BasicDataSource getBasicDataSource(
+            String url,
+            String username,
+            String password
+    ) {
+        Pattern pattern = Pattern.compile("jdbc:(\\w+?):");
+        Matcher matcher = pattern.matcher(url);
+
+        String driverClassName = "";
+        String validationQuery = "";
+        if (matcher.find()) {
+            driverClassName = databaseDriverProperties.getProperty("drivers." + matcher.group(1));
+            validationQuery = databaseDriverProperties.getProperty("validationQueries." + matcher.group(1));
+        }
+
+        BasicDataSource dataSource = dataSources.get(url);
+
+        if (dataSource == null) {
+            dataSource = new BasicDataSource();
+
+            dataSource.setDriverClassName(driverClassName);
+            dataSource.setUsername(username);
+            dataSource.setPassword(password);
+            dataSource.setUrl(url);
+            dataSource.setMaxIdle(5);
+            dataSource.setValidationQuery(validationQuery);
+
+            dataSources.put(url, dataSource);
+        }
+
+        return dataSource;
+    }
+}
---
+++ src/main/java/com/ebquery/util/StringUtil.java
@@ -0,0 +1,24 @@
+package com.EBQuery.util;
+
+import org.apache.log4j.Logger;
+import org.springframework.security.crypto.codec.Hex;
+
+import java.security.MessageDigest;
+
+public class StringUtil {
+    private static final Logger logger = Logger.getLogger(StringUtil.class);
+
+    public static String hashWithSha256(String input) {
+        String output = "";
+
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(input.getBytes("UTF-8"));
+            output = new String(Hex.encode(messageDigest.digest()));
+        } catch (Exception e) {
+            logger.debug(e);
+        }
+
+        return output;
+    }
+}
