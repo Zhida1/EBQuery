eb0c12cb4ce5c132a5b09f2237b4c5fba890363b
Y77CH <y77chen@outlook.com>
2020-10-28 07:14:19 +0700

    Add filters

---
+++ src/main/java/com/ebquery/filter/CORSFilter.java
@@ -0,0 +1,25 @@
+package com.EBQuery.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CORSFilter implements Filter {
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+        chain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+
+}
