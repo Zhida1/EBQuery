0305e02462d0bf21dca40bc533962b2e88f1b43f
Y77CH <y77chen@outlook.com>
2020-11-07 19:14:19 +0700

    API: ctrl for query

---
+++ src/main/java/com/ebquery/api/QueryController.java
@@ -0,0 +1,67 @@
+package com.EBQuery.api;
+
+import com.EBQuery.model.Query;
+import com.EBQuery.model.dao.QueryDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/v1/queries")
+public class QueryController {
+    @Autowired
+    private QueryDAO queryDAO;
+
+    @RequestMapping(value = "/findAll", method = RequestMethod.GET, produces = { "application/xml; charset=utf-8", "application/json; charset=utf-8" })
+    public @ResponseBody
+    List<Query> findAll() {
+        return queryDAO.findAll();
+    }
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = { "application/xml; charset=utf-8", "application/json; charset=utf-8" })
+	public @ResponseBody
+    Query find(@PathVariable Integer id) {
+		return queryDAO.find(id);
+	}
+
+    @RequestMapping(value = "/findByDatabaseConnectionId/{id}", method = RequestMethod.GET, produces = { "application/xml; charset=utf-8", "application/json; charset=utf-8" })
+    public @ResponseBody
+    List<Query> findByDatabaseConnectionId(@PathVariable Integer id) {
+        return queryDAO.findByDatabaseConnectionId(id);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = { "application/xml; charset=utf-8", "application/json; charset=utf-8" },
+            produces = { "application/xml; charset=utf-8", "application/json; charset=utf-8" })
+    public @ResponseBody
+    Query create(
+            @RequestBody Query query
+    ) {
+        Number id = queryDAO.insert(query);
+
+        return queryDAO.find(new Long(id.longValue()).intValue());
+    }
+
+    @RequestMapping(
+            value = "/{id}",
+            consumes = { "application/xml; charset=utf-8", "application/json; charset=utf-8" },
+            method = RequestMethod.PUT)
+    public @ResponseBody String update(
+            @PathVariable Integer id,
+            @RequestBody Query query
+    ) {
+        queryDAO.update(id, query);
+
+        return "OK";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public @ResponseBody String delete(@PathVariable Integer id) {
+        queryDAO.delete(id);
+
+        return "OK";
+    }
+}
